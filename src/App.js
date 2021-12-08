ce5cb8a2cdf4f558bfcb0b1b09088e317605e781
NeWutyteSZoxanom <mirzokhid.djamolov.97@mail.ru>
2021-12-08 11:40:17 +0500

    dsd

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import Login from "./Pages/Login";
+import Redirect from "./Pages/Redirect";
+import { Route, BrowserRouter as Router } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Route exact path="/" component={Login} />
+        <Route path="/red" component={Redirect} />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
